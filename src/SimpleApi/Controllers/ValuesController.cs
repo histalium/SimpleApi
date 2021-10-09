b456103c96aa5797908b0f32ba3af3b6f95bad69
histalium <histalium@outlook.com>
2021-10-09 12:44:13 +0200

    Initial commit

---
+++ src/SimpleApi/Controllers/ValuesController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace SimpleApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ValuesController : ControllerBase
+    {
+        public ValuesController()
+        {
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<string> Get(int id)
+        {
+            return "Histalium";
+        }
+    }
+}
---
+++ test/SimpleApi.Test/UnitTest1.cs
@@ -0,0 +1,19 @@
+using System;
+using Xunit;
+using SimpleApi.Controllers;
+
+namespace SimpleApi.Test
+{
+    public class UnitTest1
+    {
+        ValuesController controller = new ValuesController();
+
+        [Fact]
+        public void GetReturnsMyName()
+        {
+            var returnValue = controller.Get(1);
+
+            Assert.Equal("Histalium", returnValue.Value);
+        }
+    }
+}
